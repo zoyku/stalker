dbe0d774fc18fb548f192e7a64609596d6674cf0
Zeynep <zeynepbuyukbayy@gmail.com>
2022-08-03 16:26:59 +0300

    requirements.txt added

--- application/daemons/phishing_detector.py
+++ application/daemons/phishing_detector.py
@@ -1,4 +1,5 @@
 import datetime
+import time
 
 import whois as whois
 
@@ -7,33 +8,46 @@ from application.models import User, KeywordTypo, PossiblePhishing
 from application.utils.core_utils import CoreUtils
 
 
-# while True
-# mod%5
-
-
 def detect_phishings(total_number_of_daemons, daemon_number):
     app = create_app()
     with app.app_context():
-        users = User.query.filter(User.id % total_number_of_daemons==daemon_number).all()
-        tlds = CoreUtils.get_tlds()
-        for user in users:
-            keywords = KeywordTypo.query.filter_by(user_id=user.id)
-            for keyword in keywords:
-                for tld in tlds:
-                    checked_domain = keyword.typo + tld
-                    try:
-                        w = whois.whois(checked_domain)
-                    except Exception:
-                        continue
-                    else:
-                        if w.domain_name is not None:
-                            phishing_domain = PossiblePhishing(possible_phishing_domain=checked_domain,
-                                                               insert_date=datetime.datetime.utcnow(),
-                                                               update_date=datetime.datetime.utcnow(),
-                                                               from_which_typo=keyword.typo,
-                                                               user_id=user.id)
-                            db.session.add(phishing_domain)
-                            db.session.commit()
+        while True:
+            users = User.query.filter(User.id % total_number_of_daemons == daemon_number).all()
+            tlds = CoreUtils.get_tlds()
+            for user in users:
+                print("Checking for user: %s" % user.register_name)
+                keywords = KeywordTypo.query.filter_by(user_id=user.id)
+                for keyword in keywords:
+                    for tld in tlds:
+                        checked_domain = keyword.typo + tld
+                        is_exist = PossiblePhishing.query.filter_by(possible_phishing_domain=checked_domain).first()
+                        if is_exist is None:
+                            try:
+                                w = whois.whois(checked_domain)
+                            except Exception:
+                                continue
+                            else:
+                                if w.domain_name is not None:
+                                    print("found domain")
+                                    is_exist_in_ppd = PossiblePhishing.query.filter_by(
+                                        possible_phishing_domain=checked_domain).first()
+                                    if is_exist_in_ppd is None:
+                                        phishing_domain = PossiblePhishing(possible_phishing_domain=checked_domain,
+                                                                           insert_date=datetime.datetime.utcnow(),
+                                                                           update_date=datetime.datetime.utcnow(),
+                                                                           from_which_typo=keyword.typo,
+                                                                           user_id=user.id)
+                                        db.session.add(phishing_domain)
+                                        db.session.commit()
+                                        print("possible phishing domain added")
+                                    else:
+                                        continue
+                        else:
+                            continue
+
+            print("finished the job, sleeping for 60 seconds")
+            time.sleep(60)
+            print("60 seconds passed")
 
 
 if __name__ == '__main__':
--- application/utils/core_utils.py
+++ application/utils/core_utils.py
@@ -1,6 +1,6 @@
 class CoreUtils:
     @staticmethod
     def get_tlds():
-        tld_list = ['.ml', '.tk', '.com', '.org', '.net', '.gov', '.io', '.ca', '.edu', '.nl', '.eu']
+        tld_list = ['.ml', '.tk', '.com', '.org', '.net', '.gov', '.io', '.ca', '.edu', '.nl']
 
         return tld_list
