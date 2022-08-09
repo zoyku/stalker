f55f97d47e63a019141b7db5bc731443add9ca40
Zeynep <zeynepbuyukbayy@gmail.com>
2022-08-09 16:15:38 +0300

    requirements.txt added

--- application/daemons/phishing_detector.py
+++ application/daemons/phishing_detector.py
@@ -7,7 +7,7 @@ from application import create_app, db
 from application.models import User, KeywordTypo, PossiblePhishing
 from application.utils.core_utils import CoreUtils
 
-logging.basicConfig(filename='phishing_detector.log', format="[%(asctime)s] %(levelname)s %(message)s", level=logging.INFO)
+logging.basicConfig(filename='../logs/phishing_detector.log', format="[%(asctime)s] %(levelname)s %(message)s", level=logging.INFO)
 
 
 def change_date_format(domain_creation_date):
@@ -25,7 +25,7 @@ def detect_phishings(total_number_of_daemons, daemon_number):
             users = User.query.filter(User.id % total_number_of_daemons == daemon_number).all()
             tlds = CoreUtils.get_tlds()
             for user in users:
-                app.logger.info("Checking for user: %s" % user.register_name)
+                app.logger.info("Checking for keyword '%s' for user %s" % (user.keyword, user.register_name))
                 keywords = KeywordTypo.query.filter_by(user_id=user.id)
                 for keyword in keywords:
                     for tld in tlds:
@@ -35,14 +35,13 @@ def detect_phishings(total_number_of_daemons, daemon_number):
                             try:
                                 domain_info = whois.whois(checked_domain)
                             except Exception as e:
-                                app.logger.error(e)
                                 continue
                             else:
                                 if domain_info.domain_name is not None:
                                     domain_creation_time = change_date_format(domain_info.creation_date)
                                     if domain_creation_time > datetime.datetime.now() - datetime.timedelta(
                                             days=2):
-                                        app.logger.info("found domain")
+                                        app.logger.info("Found a registered possible phishing domain")
                                         is_exist_in_ppd = PossiblePhishing.query.filter_by(
                                             possible_phishing_domain=checked_domain).first()
                                         if is_exist_in_ppd is None:
@@ -54,15 +53,15 @@ def detect_phishings(total_number_of_daemons, daemon_number):
                                                                                register_name=user.register_name)
                                             db.session.add(phishing_domain)
                                             db.session.commit()
-                                            app.logger.info("possible phishing domain added")
+                                            app.logger.info("Possible phishing domain added to the database.")
                                         else:
                                             continue
                         else:
                             continue
 
-            app.logger.info("finished the job, sleeping for 60 seconds")
+            app.logger.info("Finished the job, sleeping for 60 seconds.")
             time.sleep(60)
-            app.logger.info("60 seconds passed")
+            app.logger.info("60 seconds passed.")
 
 
 if __name__ == '__main__':
